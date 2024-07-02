45977757d38e2ce20867af7be65784cf7a40a5e3
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-07-02 13:09:44 +0300

    -m

--- commitStraight.sh
+++ commitStraight.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #the  (-a) is a flag 
-git commit -a -m "Message / or Name of the file including its extension depending on the flag "
+git commit -a -m "Message / Name of the file including its extension depending on the flag"
